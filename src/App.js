fe038f7058127d65a191d90c3489f5f17a661ded
Ganapathi Senthilkumar, Harish Raju <hagana@iu.edu>
2022-11-13 16:03:54 -0500

    Added webcam component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Profile from './components/Profile.js'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Cloud Computing Project 
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <Profile />
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import Webcam from 'react-webcam'
+const WebcamComponent = () => <Webcam />
+const videoConstraints = {
+  width: 400,
+  height: 400,
+  facingMode: 'user',
+}
+const Profile = () => {
+  const [picture, setPicture] = useState('')
+  const webcamRef = React.useRef(null)
+  const capture = React.useCallback(() => {
+    const pictureSrc = webcamRef.current.getScreenshot()
+    setPicture(pictureSrc)
+  })
+  return (
+    <div>
+      <h2 className="mb-5 text-center">
+        Engineering Cloud Computing Project
+      </h2>
+      <div>
+        {picture == '' ? (
+          <Webcam
+            audio={false}
+            height={400}
+            ref={webcamRef}
+            width={400}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={picture} />
+        )}
+      </div>
+      <div>
+        {picture != '' ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              setPicture()
+            }}
+            className="btn btn-primary"
+          >
+            Retake
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              capture()
+            }}
+            className="btn btn-danger"
+          >
+            Capture and Upload
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+export default Profile
